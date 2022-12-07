cfa38b14289632632e31ede845b640e0fb6fa736
ZackAXue <100821190+ZackAXue@users.noreply.github.com>
2022-12-06 23:39:51 -0500

    compute_layer_rollout_attention

--- baselines/ViT/ViT_LRP.py
+++ baselines/ViT/ViT_LRP.py
@@ -58,6 +58,19 @@ def compute_rollout_attention(all_layer_matrices, start_layer=0):
     print(joint_attention)
     print("===========================================")
     return joint_attention
+############################################################################
+# TODO
+def compute_layer_rollout_attention(all_layer_matrices, start_layer=0):
+    # adding residual consideration
+    num_tokens = all_layer_matrices[0].shape[1]
+    batch_size = all_layer_matrices[0].shape[0]
+    eye = torch.eye(num_tokens).expand(batch_size, num_tokens, num_tokens).to(all_layer_matrices[0].device)
+    all_layer_matrices = [all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))]
+    # all_layer_matrices = [all_layer_matrices[i] / all_layer_matrices[i].sum(dim=-1, keepdim=True)
+    #                       for i in range(len(all_layer_matrices))]
+    joint_attention = all_layer_matrices[start_layer]
+    return joint_attention
+############################################################################
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, drop=0.):
